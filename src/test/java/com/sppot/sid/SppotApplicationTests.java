6e2d94b80318dc35e4a4d9b95b27b09c57440de9
unknown <you@example.com>
2020-10-16 21:25:33 +0200

    mavven

--- src/test/java/com/sppot/sid/SppotApplicationTests.java
+++ src/test/java/com/sppot/sid/SppotApplicationTests.java
@@ -1,13 +1,10 @@
 package com.sppot.sid;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SppotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
